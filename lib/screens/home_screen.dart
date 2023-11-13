e99e2a09f879a7e3ad67fb5c67cb410c5e0332f7
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-11-13 12:15:14 -0400

    add home screen file

---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,112 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_vision/flutter_vision.dart';
+import '../models/yolo_video.dart';
+import '../models/yolo_image_v8seg.dart';
+import '../models/yolo_capture_v8seg.dart';
+
+enum Options { none, imagev8seg, capturev8seg, frame }
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late FlutterVision vision;
+
+  Options option = Options.none;
+  @override
+  void initState() {
+    super.initState();
+    vision = FlutterVision();
+  }
+
+  @override
+  void dispose() async {
+    super.dispose();
+    await vision.closeTesseractModel();
+    await vision.closeYoloModel();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Row(
+          // Esto crea una fila horizontal
+          children: [
+            Icon(Icons.camera), // Esto muestra un icono de una planta
+            SizedBox(
+                width: 8), // Esto añade un espacio entre el icono y el texto
+            Text('PlantNet Sucre'), // Esto muestra el texto del título
+          ],
+        ),
+        backgroundColor: Colors.green, // Esto cambia el fondo a verde
+      ),
+      body: task(option),
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.green,
+        selectedItemColor: Colors.white,
+        //barra de menú horizontal
+        items: const [
+          //ítems de la barra
+          BottomNavigationBarItem(
+            //ítem para YoloV8seg on Frame
+            icon: Icon(Icons.home),
+            label: 'Inicio',
+          ),
+          BottomNavigationBarItem(
+            //ítem para YoloV8seg on Image
+            icon: Icon(Icons.image),
+            label: 'On Image',
+          ),
+          BottomNavigationBarItem(
+            //ítem para YoloV8seg on capture
+            icon: Icon(Icons.camera),
+            label: 'On Capture',
+          ),
+          BottomNavigationBarItem(
+            //ítem para YoloV8seg on capture
+            icon: Icon(Icons.video_call),
+            label: 'On Frame',
+          ),
+        ],
+        type: BottomNavigationBarType.fixed,
+        currentIndex: option.index, //índice del ítem seleccionado
+        onTap: (int index) {
+          //función para cambiar el ítem seleccionado
+          setState(() {
+            option = Options.values[index];
+          });
+        },
+      ),
+    );
+  }
+
+  Widget task(Options option) {
+    if (option == Options.frame) {
+      return YoloVideo(vision: vision);
+    }
+    if (option == Options.imagev8seg) {
+      return YoloImageV8Seg(vision: vision);
+    }
+    if (option == Options.capturev8seg) {
+      return YoloCaptureV8Seg(vision: vision);
+    }
+    return Center(
+      child: Stack(
+        alignment: Alignment.center,
+        children: [
+          Container(
+            color: Colors.green[200], // Esto es un color verde claro
+          ),
+          const Image(
+            image: AssetImage("assets/images/OIG3.jpeg"),
+          ),
+        ],
+      ),
+    );
+  }
+}
