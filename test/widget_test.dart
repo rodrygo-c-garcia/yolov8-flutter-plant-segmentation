a384a0398d025d52197d6dc4ff36348d2fbe6103
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-11-13 12:14:37 -0400

    import package home_screen

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:plantnet_sucre/main.dart';
+import 'package:plantnet_sucre/screens/home_screen.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
