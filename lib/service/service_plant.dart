a702709f8ae7828f60c6f52bbad2135bca7c2626
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-11-18 16:01:48 -0400

    feat: add service plant

---
+++ lib/service/service_plant.dart
@@ -0,0 +1,26 @@
+// Crear una clase de servicio que gestione las variables globales
+class ServicioPlanta {
+  // Usar el patrón singleton para crear una única instancia de la clase
+  static final ServicioPlanta _instance = ServicioPlanta._internal();
+
+  // Crear el constructor privado
+  ServicioPlanta._internal();
+
+  // Crear el método que devuelve la instancia de la clase
+  factory ServicioPlanta() {
+    return _instance;
+  }
+
+  // Crear los atributos que quieres usar como variables globales
+  String planta = "", descripcion = "";
+
+  // Crear la lista de objetos DolenciaPlanta
+  List<DolenciaPlanta> lista = [];
+}
+
+class DolenciaPlanta {
+  final String dolencia;
+  final String planta;
+
+  DolenciaPlanta(this.dolencia, this.planta);
+}
