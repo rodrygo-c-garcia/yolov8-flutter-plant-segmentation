82b425532149facdaf500c4debac3dfffd3c5efa
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-11-23 20:00:24 -0400

    feat: add import dependency http

--- lib/screens/detail_plant.dart
+++ lib/screens/detail_plant.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import '../class/plant.dart';
 
 class DetailPlant extends StatefulWidget {
   final String tag;
