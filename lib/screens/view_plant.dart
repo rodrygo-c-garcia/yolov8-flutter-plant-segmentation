bfe21c5cae22a5bbd00d4918253970da7e462a7b
Ariel Rodrigo Colque Garcia <colqueariel023@gmail.com>
2023-12-13 18:07:55 -0400

    feat: add image option

--- lib/screens/view_plant.dart
+++ lib/screens/view_plant.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import '../service/service_plant.dart';
 import 'dart:typed_data';
+import 'package:http/http.dart' as http;
+import '../class/dolencia.dart';
+import 'dart:convert';
 
 class ViewPlant extends StatefulWidget {
   final Uint8List image; // El archivo con la imagen recortada
@@ -12,6 +15,13 @@ class ViewPlant extends StatefulWidget {
 
 class ViewPlantInfo extends State<ViewPlant> {
   ServicioPlanta service = ServicioPlanta();
+  late Future<Dolencia> dolencia;
+
+  @override
+  void initState() {
+    super.initState();
+    dolencia = getDolencia(service.dolencia);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,23 +30,23 @@ class ViewPlantInfo extends State<ViewPlant> {
     String imageUrl2 = "";
     // Usa un switch para asignar las URLs según el valor de service.planta
     switch (service.planta) {
-      case "apacarro":
+      case "alcaparro":
         imageUrl1 =
-            'assets/images/apacarro/apacarro1.jpg'; // La ruta de la primera imagen local para apacarro
+            'assets/images/alparraco/alparraco1.jpeg'; // La ruta de la primera imagen local para apacarro
         imageUrl2 =
-            'assets/images/apacarro/apacarro2.jpg'; // La ruta de la segunda imagen local para apacarro
+            'assets/images/alparraco/alparraco2.jpeg'; // La ruta de la segunda imagen local para apacarro
         break;
       case "jengibre":
         imageUrl1 =
-            'assets/images/jengibre/jengibre1.jpg'; // La ruta de la primera imagen local para jengibre
+            'assets/images/jengibre/jengibre1.jpeg'; // La ruta de la primera imagen local para jengibre
         imageUrl2 =
-            'assets/images/jengibre/jengibre2.jpg'; // La ruta de la segunda imagen local para jengibre
+            'assets/images/jengibre/jengibre2.jpeg'; // La ruta de la segunda imagen local para jengibre
         break;
-      case "menta":
+      case "menta_silvestre":
         imageUrl1 =
-            'assets/images/menta/menta1.jpg'; // La ruta de la primera imagen local para menta
+            'assets/images/menta/menta1.jpeg'; // La ruta de la primera imagen local para menta
         imageUrl2 =
-            'assets/images/menta/menta2.jpg'; // La ruta de la segunda imagen local para menta
+            'assets/images/menta/menta2.jpeg'; // La ruta de la segunda imagen local para menta
         break;
       case "eucalipto":
         imageUrl1 =
@@ -46,21 +56,21 @@ class ViewPlantInfo extends State<ViewPlant> {
         break;
       case "malva":
         imageUrl1 =
-            'assets/images/malva/malva1.jpg'; // La ruta de la primera imagen local para malva
+            'assets/images/malva/malva1.jpeg'; // La ruta de la primera imagen local para malva
         imageUrl2 =
-            'assets/images/malva/malva2.jpg'; // La ruta de la segunda imagen local para malva
+            'assets/images/malva/malva2.jpeg'; // La ruta de la segunda imagen local para malva
         break;
       case "manzanilla":
         imageUrl1 =
-            'assets/images/manzanilla/manzanilla1.jpg'; // La ruta de la primera imagen local para manzanilla
+            'assets/images/manzanilla/manzanilla1.jpeg'; // La ruta de la primera imagen local para manzanilla
         imageUrl2 =
-            'assets/images/manzanilla/manzanilla2.jpg'; // La ruta de la segunda imagen local para manzanilla
+            'assets/images/manzanilla/manzanilla2.jpeg'; // La ruta de la segunda imagen local para manzanilla
         break;
       case "siempreviva":
         imageUrl1 =
-            'assets/images/siempreviva/siempreviva1.jpg'; // La ruta de la primera imagen local para siempreviva
+            'assets/images/siempreviva/siempreviva1.jpeg'; // La ruta de la primera imagen local para siempreviva
         imageUrl2 =
-            'assets/images/siempreviva/siempreviva2.jpg'; // La ruta de la segunda imagen local para siempreviva
+            'assets/images/siempreviva/siempreviva2.jpeg'; // La ruta de la segunda imagen local para siempreviva
         break;
       default:
         imageUrl1 = ""; // La ruta por defecto para la primera imagen local
@@ -136,17 +146,217 @@ class ViewPlantInfo extends State<ViewPlant> {
                 child: Text(
                   service.planta.toUpperCase(),
                   style: const TextStyle(
-                      fontSize: 24, fontWeight: FontWeight.bold),
+                      fontSize: 20, fontWeight: FontWeight.bold),
                 ),
               ),
               // Muestra la descripción de la planta usando service.descripcion
               Expanded(
-                child: SingleChildScrollView(
-                  child: Text(
-                    service.descripcion,
-                    style: const TextStyle(
-                        fontSize: 15, fontWeight: FontWeight.normal),
-                  ),
+                child: FutureBuilder<Dolencia>(
+                  future: dolencia,
+                  builder: (context, snapshot) {
+                    // Si la petición está en progreso, muestra un indicador de carga
+                    if (snapshot.connectionState == ConnectionState.waiting) {
+                      return const Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    }
+                    // Si la petición falló, muestra un mensaje de error
+                    if (snapshot.hasError) {
+                      return Center(
+                        child: Text('Error: ${snapshot.error}'),
+                      );
+                    }
+                    // Si la petición fue exitosa, muestra los datos de la planta
+                    if (snapshot.hasData) {
+                      // Obtiene el objeto de tipo Planta del snapshot
+                      var dolencia = snapshot.data!;
+                      // Retorna un widget que muestre los datos de la planta
+                      return SingleChildScrollView(
+                        child: Padding(
+                          padding: const EdgeInsets.all(16.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              // Muestra el nombre común de la planta
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    // Muestra el título de la dolencia
+                                    Text(
+                                      dolencia.titulo,
+                                      style: const TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                    // Muestra la introducción de la dolencia
+                                    Text(
+                                      dolencia.introduccion,
+                                      style: const TextStyle(
+                                        fontSize: 16,
+                                        fontStyle: FontStyle.italic,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+
+                              // Muestra los beneficios de la dolencia
+                              const Text(
+                                'Beneficios:',
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              ListView.builder(
+                                shrinkWrap: true,
+                                physics: const NeverScrollableScrollPhysics(),
+                                itemCount: dolencia.beneficios.length,
+                                itemBuilder: (context, index) {
+                                  // Obtiene el valor de cada beneficio
+                                  var valor = dolencia.beneficios[index];
+                                  // Retorna un widget que muestre el valor de cada beneficio con un icono
+                                  return ListTile(
+                                    leading: const Icon(Icons.check),
+                                    title: Text(
+                                      valor,
+                                      style: const TextStyle(
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                  );
+                                },
+                              ),
+// Muestra las formas de uso de la dolencia
+                              const Text(
+                                'Formas de uso:',
+                                style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+
+                              ListView.builder(
+                                shrinkWrap: true,
+                                physics: const NeverScrollableScrollPhysics(),
+                                itemCount: dolencia.formas_de_uso.length,
+                                itemBuilder: (context, index) {
+                                  // Obtiene el objeto de tipo FormaDeUso de cada forma de uso
+                                  var forma = dolencia.formas_de_uso[index];
+                                  // Retorna un widget que muestre el nombre, las instrucciones y la recomendación de cada forma de uso con un icono
+                                  return ExpansionTile(
+                                    leading: const Icon(Icons.medical_services),
+                                    title: Text(forma.nombre),
+                                    children: [
+                                      // Muestra las instrucciones de cada forma de uso
+                                      const Text(
+                                        'Instrucciones:',
+                                        style: TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      // Usa un widget ListView.builder para mostrar cada instrucción con un icono
+                                      ListView.builder(
+                                        shrinkWrap: true,
+                                        physics:
+                                            const NeverScrollableScrollPhysics(),
+                                        itemCount: forma.instrucciones.length,
+                                        itemBuilder: (context, index) {
+                                          // Obtiene el valor de cada instrucción
+                                          var valor =
+                                              forma.instrucciones[index];
+                                          // Retorna un widget que muestre el valor de cada instrucción con un icono
+                                          return ListTile(
+                                            leading:
+                                                const Icon(Icons.arrow_right),
+                                            title: Text(valor),
+                                          );
+                                        },
+                                      ),
+                                      // Muestra la recomendación de cada forma de uso
+                                      const Text(
+                                        'Recomendación:',
+                                        style: TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                      ),
+                                      Padding(
+                                        padding: const EdgeInsets.fromLTRB(
+                                            0, 0, 0, 10),
+                                        child: Text(
+                                          forma.recomendacion,
+                                          style: const TextStyle(
+                                            fontSize: 16,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  );
+                                },
+                              ),
+                              const Padding(
+                                padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
+                                child: Text(
+                                  'Precauciones:',
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.fromLTRB(0, 0, 0, 10),
+                                child: Text(
+                                  dolencia.precauciones,
+                                  style: const TextStyle(
+                                    fontSize: 16,
+                                  ),
+                                ),
+                              ),
+
+                              const Padding(
+                                padding: EdgeInsets.fromLTRB(0, 0, 0, 10),
+                                child: Text(
+                                  'Consejos adicionales:',
+                                  style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                              ),
+                              ListView.builder(
+                                shrinkWrap: true,
+                                physics: const NeverScrollableScrollPhysics(),
+                                itemCount: dolencia.consejos_adicionales.length,
+                                itemBuilder: (context, index) {
+                                  // Obtiene el valor de cada consejo adicional
+                                  var valor =
+                                      dolencia.consejos_adicionales[index];
+                                  // Retorna un widget que muestre el valor de cada consejo adicional con un icono
+                                  return ListTile(
+                                    leading: const Icon(Icons.lightbulb),
+                                    title: Text(
+                                      valor,
+                                      style: const TextStyle(
+                                        fontSize: 16,
+                                      ),
+                                    ),
+                                  );
+                                },
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    }
+                    // Si no hay datos, muestra un widget vacío
+                    return const SizedBox.shrink();
+                  },
                 ),
               ),
             ],
@@ -154,5 +364,20 @@ class ViewPlantInfo extends State<ViewPlant> {
         ),
       ),
     );
+  }
+
+  // Crea una función que haga la petición a la API y que retorne un objeto de tipo Future<Dolencia>
+  Future<Dolencia> getDolencia(String dolor) async {
+    // Define la URL de la API con el parámetro dolor
+    var url =
+        'https://ez6v5h6xf9.execute-api.eu-north-1.amazonaws.com/dolencia?dolor=$dolor';
+    // Usa el método http.get para hacer la petición
+    var response = await http.get(Uri.parse(url));
+    // Usa el método jsonDecode para convertir la respuesta en un mapa de tipo Map<String, dynamic>
+    var json = jsonDecode(response.body);
+    // Usa el método Dolencia.fromJson para convertir el mapa en un objeto de tipo Dolencia
+    var dolencia = Dolencia.fromJson(json);
+    // Retorna el objeto de tipo Dolencia
+    return dolencia;
   }
 }
